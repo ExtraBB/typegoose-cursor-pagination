2a790007b55d7f29f7eda681924759d6a7ad6ad6
Bruno Carvalhal <private>
2023-06-16 22:09:44 +0200

    Add explain functionality for debugging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Schema, PopulateOptions } from "mongoose";
 import { generateCursorQuery, generateSort } from "./query";
 import { prepareResponse } from "./response";
-import { IPaginateOptions, IPaginateResult } from "./types";
+import { IPaginateOptions, IPaginateResult, VerboseMode } from "./types";
 
 export interface IPluginOptions {
     dontReturnTotalDocs?: boolean;
@@ -15,14 +15,7 @@ export interface IPluginOptions {
  */
 export default function (schema: Schema, pluginOptions?: IPluginOptions) {
 
-    /**
-     * Peform a paginated find() request
-     * @param {IPaginateOptions} options the pagination options
-     * @param {Object} [_query] the mongo query
-     * @param {Object} [_projection] the mongo projection
-     * @param {string | PopulateOptions | (string | PopulateOptions)[]} [_populate] the mongo populate
-     */
-    async function findPaged<T>(options: IPaginateOptions, _query?: Object, _projection?: Object, _populate?: string | PopulateOptions | (string | PopulateOptions)[]): Promise<IPaginateResult<T>> {
+    function createFindPromise(mongoObject: any, options: IPaginateOptions, _query?: Object, _projection?: Object) {
         // Determine sort
         const sort = generateSort(options);
 
@@ -36,7 +29,21 @@ export default function (schema: Schema, pluginOptions?: IPluginOptions) {
         const query = { $and: [generateCursorQuery(options), _query || {}] };
 
         // Request one extra result to check for a next/previous
-        const docs = await this.find(query, _projection).sort(sort).limit(unlimited ? 0 : options.limit + 1).populate(_populate || []);
+        const promise = mongoObject.find(query, _projection).sort(sort).limit(unlimited ? 0 : options.limit + 1);
+
+        return promise;
+    }
+
+    /**
+     * Peform a paginated find() request
+     * @param {IPaginateOptions} options the pagination options
+     * @param {Object} [_query] the mongo query
+     * @param {Object} [_projection] the mongo projection
+     * @param {string | PopulateOptions | (string | PopulateOptions)[]} [_populate] the mongo populate
+     */
+    async function findPaged<T>(options: IPaginateOptions, _query?: Object, _projection?: Object, _populate?: string | PopulateOptions | (string | PopulateOptions)[]): Promise<IPaginateResult<T>> {
+        // Find and populate docs
+        const docs = await createFindPromise(this, options, _query, _projection).populate(_populate || []);
 
         if (pluginOptions && pluginOptions.dontReturnTotalDocs) {
             return prepareResponse<T>(docs, options);
@@ -46,7 +53,19 @@ export default function (schema: Schema, pluginOptions?: IPluginOptions) {
         }
     }
 
+    /**
+     * Explains a paginated find() request
+     * @param {IPaginateOptions} options the pagination options
+     * @param {VerboseMode} verbose the verbosity mode for explain()
+     * @param {Object} [_query] the mongo query
+     * @param {Object} [_projection] the mongo projection
+     */
+    async function findPagedExplain(options: IPaginateOptions,  verbose?: VerboseMode, _query?: Object, _projection?: Object): Promise<any> {
+        return await createFindPromise(this, options, _query, _projection).explain(verbose);
+    }
+
     schema.statics.findPaged = findPaged;
+    schema.statics.findPagedExplain = findPagedExplain;
 }
 
 export * from "./types";
